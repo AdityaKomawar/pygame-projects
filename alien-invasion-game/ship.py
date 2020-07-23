87d27965e55b2a99ee0a9f218ad67ab113a2e666
Aditya Komawar <62996831+AdityaKomawar@users.noreply.github.com>
2020-07-23 20:39:24 +0530

    files added

---
+++ alien-invasion-game/ship.py
@@ -0,0 +1,36 @@
+
+import pygame
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.settings = ai_game.settings
+
+        self.x = float(self.rect.x)
+
+        self.move_right = False
+        self.move_left = False
+
+
+    def update(self):
+        if self.move_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.move_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
